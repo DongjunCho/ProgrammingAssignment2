4d1a4988b10b7d0d33ba93cfad0dcd100a6d2873
DongjunCho <67394778+DongjunCho@users.noreply.github.com>
2020-06-26 13:26:59 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -9,19 +9,24 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
+  ## set the matrix value
   set <- function(j){
     x <<- j
     i <<- NULL
   }
+  ## get the matrix value
   get <- function(){
     x 
   }
+  ## set the inverse matrix vlaue
   setInverse<- function(inverse){
-    x <<- inverse
-  }
+    i <<- inverse
+  } 
+  ## get the inverse matrix value
   getInverse <- function(){
-    x
+    i
   }
+  ## return a list
   list(set = set, get= get, 
        setInverse = setInverse, 
        getInverse= getInverse)
@@ -37,17 +42,20 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   i <- x$getInverse()
-  ##if the inverse is null, skip and show the message
+  ##Chekc whether the inverse is already calculated or not.
+  ##cahced
   if(!is.null(i)){
-    message("getting cached data")
-    return(i)
+      message("getting cached data")
+      return(i)
   }
   data <- x$get()
+
   ##solve() function
   ## Computing inverse of square matrix
   ##If X is a square invertible matrix, 
   ## then solve(X) returns its inverse.
   i <- solve(data, ...)
+  
   x$setInverse(i)
   i
 }
